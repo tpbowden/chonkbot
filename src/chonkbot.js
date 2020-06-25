147e71b6f395712df035d454d392fab9f50bf76a
Tom Bowden <tom.bowden@nested.com>
2020-06-25 12:37:56 +0100

    Pretty

--- src/chonkbot.js
+++ src/chonkbot.js
@@ -1,11 +1,14 @@
 const { calculateChonk } = require("./chonk");
 
+const context = github.context;
 
 const {
   additions,
   deletions,
   changed_files: files,
 } = context.payload.pull_request;
+
+console.log(JSON.stringify(context, null, 2));
 
 context.github.repos.createStatus({
   owner: context.payload.pull_request.head.repo.owner.login,
