7c6e8b287a2bfdf23b5dcc5d305e5eaa9eb9b32c
Tom Bowden <tom.bowden@nested.com>
2020-06-29 12:49:11 +0100

    Try using consistent octokit version (#197)
    * Try using consistent octokit version
    
    * oops

--- src/action.js
+++ src/action.js
@@ -1,8 +1,11 @@
+const { Octokit } = require("@octokit/rest");
 const core = require("@actions/core");
 const github = require("@actions/github");
 const { createStatus } = require("./core");
 
 const token = core.getInput("token", { required: true });
-const octokit = github.getOctokit(token);
+const app = new Octokit({
+  auth: token,
+});
 
-createStatus(github.context.payload, octokit);
+createStatus(github.context.payload, app);
